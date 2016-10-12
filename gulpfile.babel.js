de61c479e5ec4f49c1467a65181991929c1bf7f7
Martin Benes <martin.benes66@gmail.com>
2016-10-12 15:25:42 -0400

    added webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,6 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import eslint from 'gulp-eslint';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import source from 'vinyl-source-stream';
 
 gulp.task('build-server', ['lint'], () =>
   gulp.src([
@@ -19,6 +16,7 @@ gulp.task('build-server', ['lint'], () =>
 gulp.task('lint', () =>
   gulp.src([
     'gulpfile.babel.js',
+      'webpack.config.babel.js',
     'src/**/*.js',
     'src/**/*.jsx',
   ])
@@ -27,10 +25,3 @@ gulp.task('lint', () =>
     .pipe(eslint.failAfterError())
 );
 
-gulp.task('build-client', ['lint'], () =>
-  browserify({ entries: './src/client/app.jsx', debug: true })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('client-bundle.js'))
-    .pipe(gulp.dest('dist'))
-);
