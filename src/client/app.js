0b8306fe3b3a43f9f5ada8216e8752c36cb76498
Martin Benes <martin.benes66@gmail.com>
2016-10-12 13:06:06 -0400

    updated dog.js and added react

---
+++ src/client/app.js
@@ -0,0 +1,5 @@
+import Dog from '../shared/dog';
+
+const browserMax = new Dog('Max for the Browser');
+
+document.write(browserMax.bark());
---
+++ src/server/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+import Dog from '../shared/dog';
+
+const max = new Dog('Max');
+
+
+console.log(max.bark());
